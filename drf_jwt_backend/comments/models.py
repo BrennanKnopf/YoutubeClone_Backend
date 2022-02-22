691a3b3d7ee5ef4fa7835697ae5b5c914ad783c7
BrennanKnopf <brennanknopf@gmail.com>
2022-02-22 16:55:17 -0600

    fixing merge conflicts

--- drf_jwt_backend/comments/models.py
+++ drf_jwt_backend/comments/models.py
@@ -7,8 +7,8 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=50)
     text = models.CharField(max_length=100)
-    likes = models.IntegerField(max_length=100)
-    dislikes = models.IntegerField(max_length=100)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
