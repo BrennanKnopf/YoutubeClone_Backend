61567a1998b116578183dd366d0f50271fd50238
TylerHardiman <Fulldadmode89@gmail.com>
2022-02-22 16:58:07 -0500

    Resolved serializer and url pathing.

--- drf_jwt_backend/comments/views.py
+++ drf_jwt_backend/comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view,permission_classes
+from .models import Comment
+from .serializers import CommentSerializer
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+
+class CommentList(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
