c3467ac2eff80ce44971e78a8e7b375ed1ab8daf
BrennanKnopf <brennanknopf@gmail.com>
2022-02-22 15:05:29 -0600

    Created comments app

---
+++ drf_jwt_backend/comments/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=50)
+    text = models.CharField(max_length=100)
+    likes = models.IntegerField(max_length=100)
+    dislikes = models.IntegerField(max_length=100)
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+
